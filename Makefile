ryanfb/latinocr-lattraining

Initial rules and modifications for adding OpenGreekAndLatin corpora

Makefile
@@ -2,6 +2,9 @@ RIGAUDONURL = https://github.com/brobertson/rigaudon/raw/master/Dictionaries/gre
 CORPUSURL = http://www.perseus.tufts.edu/hopper/opensource/downloads/texts/hopper-texts-GreekRoman.tar.gz
 # CORPUSURL = http://ancientgreekocr.org/archived/hopper-texts-GreekRoman.tar.gz # backup copy
 UTFSRC = tools/libutf/rune.c tools/libutf/utf.c
+OPENGREEKANDLATIN_REPOS = \
+	csel-dev \
+	patrologia_latina-dev
 
 AMBIGS = \
 	common.unicharambigs \
@@ -16,11 +19,23 @@ corpus:
 	cd $@ ; wget -O - $(CORPUSURL) \
 	| zcat | tar x
 
+opengreekandlatin:
+	mkdir -p $@
+	for i in $(OPENGREEKANDLATIN_REPOS); do \
+		cd $@; wget -O - https://github.com/OpenGreekAndLatin/$$i/tarball/master | zcat | tar x; \
+	done
+
 greek_and_latin.txt:
 	wget $(RIGAUDONURL)
 
+wordlist.opengreekandlatin: tools/wordlistfromperseus.sh opengreekandlatin
+	tools/wordlistfromperseus.sh opengreekandlatin "*.xml" > $@
+
+lat.opengreekandlatin.freq.txt: tools/wordlistparsefreq.sh wordlist.opengreekandlatin
+	tools/wordlistparsefreq.sh < wordlist.opengreekandlatin > lat.opengreekandlatin.freq.txt
+
 wordlist.perseus: tools/wordlistfromperseus.sh corpus
-	tools/wordlistfromperseus.sh corpus > $@
+	tools/wordlistfromperseus.sh corpus "*_lat.xml" > $@
 
 wordlist.rigaudon: tools/wordlistfromrigaudon.sh greek_and_latin.txt
 	tools/wordlistfromrigaudon.sh < greek_and_latin.txt > $@
@@ -34,7 +49,13 @@ lat.rigaudon.word.txt: tools/rigaudonparseword.sh wordlist.rigaudon
 lat.perseus.word.txt: tools/wordlistparseword.sh wordlist.perseus
 	tools/wordlistparseword.sh < wordlist.perseus > $@
 
+lat.opengreekandlatin.word.txt: tools/wordlistparseword.sh wordlist.opengreekandlatin
+	tools/wordlistparseword.sh < wordlist.opengreekandlatin > $@
+
 lat.word.txt: lat.perseus.word.txt lat.rigaudon.word.txt
+	LC_ALL=C cat $^ | sort | uniq > $@
+
+lat.word.all.txt: lat.perseus.word.txt lat.rigaudon.word.txt lat.opengreekandlatin.word.txt
 	LC_ALL=C cat $^ | sort | uniq > $@
 
 seed:

ryanfb/ancientgreekocr-grctraining

(cherry picked from commit 86be6f54c9b81ec67e5996d321b559a3edf854be)
Conflicts:
	Makefile
	tools/wordlistfromperseus.sh

Makefile
@@ -2,6 +2,16 @@ RIGAUDONURL = https://github.com/brobertson/rigaudon/raw/master/Dictionaries/gre
 CORPUSURL = http://www.perseus.tufts.edu/hopper/opensource/downloads/texts/hopper-texts-GreekRoman.tar.gz
 # CORPUSURL = http://ancientgreekocr.org/archived/hopper-texts-GreekRoman.tar.gz # backup copy
 UTFSRC = tools/libutf/rune.c tools/libutf/utf.c
+OPENGREEKANDLATIN_REPOS = \
+	athenaeus-dev \
+	church_fathers-dev \
+	misc-dev \
+	fragmentary-dev \
+	dfhg-dev \
+	libanius-dev \
+	philo-dev \
+	catenae-dev \
+	cag-dev
 
 AMBIGS = \
 	unicharambigs.accent \
@@ -22,13 +32,25 @@ corpus:
 	cd $@ ; wget -O - $(CORPUSURL) \
 	| zcat | tar x
 
+opengreekandlatin:
+	mkdir -p $@
+	cd $@; for i in $(OPENGREEKANDLATIN_REPOS); do \
+		wget -O - https://github.com/OpenGreekAndLatin/$$i/tarball/master | zcat | tar x; \
+	done
+
 greek_and_latin.txt:
 	wget $(RIGAUDONURL)
 
-wordlist.perseus: tools/wordlistfromperseus.sh  tools/betacode2utf8.sh corpus
-	tools/wordlistfromperseus.sh corpus > wordlist-betacode
-	tools/betacode2utf8.sh wordlist-betacode > $@
+wordlist.perseus: tools/wordlistfromperseus.sh  tools/betacode2utf8.sh tools/striplineswithnonmatchingchars.sh corpus
+	tools/wordlistfromperseus.sh corpus "*_gk.xml" > wordlist-betacode
+	tools/betacode2utf8.sh wordlist-betacode | tools/striplineswithnonmatchingchars.sh allchars.txt > $@
 	rm wordlist-betacode
+
+wordlist.opengreekandlatin: tools/wordlistfromperseus.sh tools/striplineswithnonmatchingchars.sh opengreekandlatin
+	tools/wordlistfromperseus.sh opengreekandlatin "*.xml" | tools/striplineswithnonmatchingchars.sh allchars.txt > $@
+
+grc.opengreekandlatin.freq.txt: tools/wordlistparsefreq.sh wordlist.opengreekandlatin
+	tools/wordlistparsefreq.sh < wordlist.opengreekandlatin > $@
 
 wordlist.rigaudon: tools/wordlistfromrigaudon.sh greek_and_latin.txt
 	tools/wordlistfromrigaudon.sh < greek_and_latin.txt > $@
@@ -44,6 +66,12 @@ grc.perseus.word.txt: tools/wordlistparseword.sh wordlist.perseus
 
 grc.word.txt: grc.rigaudon.word.txt grc.perseus.word.txt
 	cat $^ | LC_ALL="C" sort | LC_ALL="C" uniq > $@
+
+grc.opengreekandlatin.word.txt: tools/wordlistparseword.sh wordlist.opengreekandlatin
+	tools/wordlistparseword.sh < wordlist.opengreekandlatin > $@
+
+grc.word.all.txt: grc.perseus.word.txt grc.rigaudon.word.txt grc.opengreekandlatin.word.txt
+	cat $^ | LC_ALL='C' sort | LC_ALL='C' uniq > $@
 
 seed:
 	dd if=/dev/urandom of=$@ bs=1024 count=1536