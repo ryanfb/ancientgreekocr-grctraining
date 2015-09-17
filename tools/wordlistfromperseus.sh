f3a9fd297642c8cff2e9644e37b7860567a23926
Nick White <git@njw.name>
2015-09-17 14:28:50 +0100

    Remove lone diacritical characters from perseus words

--- tools/wordlistfromperseus.sh
+++ tools/wordlistfromperseus.sh
@@ -8,13 +8,16 @@ Outputs a list of all Greek words encountered in a Perseus corpus."
 test $# -ne 1 && echo "$usage" && exit 1
 
 find "$1" -type f -name '*-grc?.xml' | LC_ALL=C sort | while read i; do
-	# Strip XML, print one word per line, remove final
-	# punctuation characters, and ensure apostrophe
-	# characters are ancient greek.
+	# - Strip XML
+	# - Print one word per line
+	# - Remove characters that shouldn't be present (lone diacritical characters)
+	# - Remove final punctuation characters
+	# - Ensure apostrophe characters are ancient greek
 	cat "$i" \
 	| sed '1,/<body>/ d; /<\/body>/,$ d' \
 	| sed 's/<[^>]*>//g; s/\&[^;]*;//g' \
 	| tr ' ' '\n' \
+	| sed '/[᾽ι῀῁῝῞῟῭΅`´῾]/d' \
 	| sed 's/ʼ/᾿/g' \
 	| sed 's/[.,·;]$//g' \
 	| sed '/^$/d'
